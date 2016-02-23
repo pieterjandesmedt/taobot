aff65847733f8e89df185ef96c52c5ad4557e4a0
Pieter Jan De Smedt <pieterjan@digitalcomet.be>
2016-02-24 00:14:29 +0100

    chore: set up http listener to avoid Heroku $PORT error

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var settings = {
-    token: 'xoxb-22739892000-D2P2hTlYC8Wcq4YWPxxzDTdz',
-    name: 'taobot'
+	token: 'xoxb-22739892000-D2P2hTlYC8Wcq4YWPxxzDTdz',
+	name: 'taobot'
 };
 
 var TaoBot = require('./taobot');
 var taobot = new TaoBot(settings);
 
 taobot.run();
+
+//For avoiding Heroku $PORT error
+var http = require('http');
+http.createServer(function (req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+	});
+	res.send('{ "message": "TaoBot is running" }');
+}).listen(process.env.PORT || 5000);
